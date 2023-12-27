ac5f30a94df573650a97e6798a90aa07520e03d4
Shivam Pandey <ashishivam2003@gmail.com>
2023-12-27 20:51:07 +0530

    add files

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const mongoose=require('mongoose');
+const plm=require("passport-local-mongoose");
+mongoose.connect("mongodb://0.0.0.0:27017/instaclone");
+const userSchema=mongoose.Schema({
+  username:String,
+  name: String,
+  email:String,
+  password: String,
+  profileImage: String,
+  bio: String,
+  posts: [{type: mongoose.Schema.Types.ObjectId, ref:"post"}],
 });
+userSchema.plugin(plm);
 
-module.exports = router;
+module.exports=mongoose.model("user",userSchema);
